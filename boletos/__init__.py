8de62da4999a6347ff425c1f99c9d97e010a44e1
Guilherme Dantas <guidanoli@proton.me>
2024-02-15 14:44:37 -0300

    feat: add home page

--- boletos/__init__.py
+++ boletos/__init__.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask
+from flask import Flask, render_template
 
 
 def create_app(test_config=None):
@@ -24,12 +24,11 @@ def create_app(test_config=None):
     except OSError:
         pass
 
-    # a simple page that says hello
-    @app.route('/hello')
-    def hello():
-        return 'Hello, World!'
-
     from . import db
     db.init_app(app)
+
+    @app.route('/')
+    def home():
+        return render_template('home.html')
 
     return app
