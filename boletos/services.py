40b54732bc64795f82a315677aeaf322456bb448
Guilherme Dantas <guidanoli@proton.me>
2024-02-15 19:47:17 -0300

    feat: simplify services (no freq or active)

--- boletos/services.py
+++ boletos/services.py
@@ -4,38 +4,30 @@ from boletos.db import get_db
 
 bp = Blueprint('services', __name__, url_prefix='/services')
 
-freq2str = {'M': 'Mensal', 'Y': 'Anual'}
-
 @bp.route('/')
 def index():
     db = get_db()
     services = db.execute(
-        'SELECT name, freq FROM service'
+        'SELECT name FROM service'
         ' ORDER BY name'
     ).fetchall()
-    return render_template('services/index.html', services=services, freq2str=freq2str)
+    return render_template('services/index.html', services=services)
 
 @bp.route('/register', methods=('GET', 'POST'))
 def register():
     if request.method == 'POST':
         name = request.form['name']
-        freq = request.form['freq']
         db = get_db()
         error = None
 
         if not name:
             error = 'O nome do serviço é obrigatório.'
-        elif not freq:
-            error = 'A frequência de cobrança é obrigatória.'
-
-        if freq not in ('M', 'Y'):
-            error = 'A frequência de cobrança deve ser mensal ou anual.'
 
         if error is None:
             try:
                 db.execute(
-                    'INSERT INTO service (name, freq) VALUES (?, ?)',
-                    (name, freq)
+                    'INSERT INTO service (name) VALUES (?)',
+                    (name,)
                 )
                 db.commit()
             except db.IntegrityError:
