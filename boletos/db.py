18194124be8ef56505ca6c008d0eaaabfb4458e2
Guilherme Dantas <guidanoli@proton.me>
2024-02-17 00:45:49 -0300

    feat: simplify get_services (use view)

--- boletos/db.py
+++ boletos/db.py
@@ -54,12 +54,4 @@ def get_boleto(service_id, boleto_id):
 
 
 def get_services():
-    return get_db().execute('''
-        SELECT id, name, last_payment_ts
-        FROM service
-        LEFT OUTER JOIN (SELECT service_id, MAX(payment_ts) as last_payment_ts
-                         FROM boleto
-                         GROUP BY service_id)
-        ON id = service_id
-        ORDER BY last_payment_ts
- ''').fetchall()
+    return get_db().execute('SELECT * FROM services_statistics').fetchall()
