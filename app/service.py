68c7ada7188dad049d2716eda0c4d9166fdf817a
Guilherme Dantas <guidanoli@proton.me>
2024-02-20 22:02:39 -0300

    feat: make `/service/new` functional

--- app/service.py
+++ app/service.py
@@ -1,11 +1,35 @@
 import flask
+from flask import Blueprint, request, render_template, redirect, flash, url_for
 
 from . import payment
+from boletos.db import get_db
 
-bp = flask.Blueprint('service', __name__, url_prefix='/service')
+bp = Blueprint('service', __name__, url_prefix='/service')
 
 bp.register_blueprint(payment.bp)
 
-@bp.route('/new')
+@bp.route('/new', methods=('GET', 'POST'))
 def new():
-    return flask.render_template('service/new.html')
+    if request.method == 'POST':
+        name = request.form.get('name')
+        db = get_db()
+        error = None
+
+        if not name:
+            error = 'A name is required.'
+
+        if error is None:
+            try:
+                db.execute(
+                    'INSERT INTO service (name) VALUES (?)',
+                    (name, )
+                )
+                db.commit()
+            except db.IntegrityError:
+                error = 'A service with this name already exists.'
+            else:
+                return redirect(url_for('index'))
+
+        flash(error)
+
+    return render_template('service/new.html')
