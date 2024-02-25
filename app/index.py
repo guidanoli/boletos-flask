4e056c31107e7871a513e177a825a539f6d2971b
Guilherme Dantas <guidanoli@proton.me>
2024-02-25 11:37:43 -0300

    feat: store `show_inactive` in session

--- app/index.py
+++ app/index.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, redirect, session, url_for
 
 from app.db import get_services
 
@@ -12,5 +12,17 @@ def index():
     services = get_services()
     kwargs = {}
     kwargs['services'] = services
-    kwargs['show_inactive'] = request.args.get('show_inactive')
+    kwargs['show_inactive'] = session.get('show_inactive')
     return render_template('index.html', **kwargs)
+
+
+@bp.route('/inactive/show')
+def show_inactive():
+    session['show_inactive'] = True
+    return redirect(url_for('index'))
+
+
+@bp.route('/inactive/hide')
+def hide_inactive():
+    session.pop('show_inactive', None)
+    return redirect(url_for('index'))
