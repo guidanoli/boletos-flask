f662e5870cdcd7b106ef9d10465b6f1f50e961f6
Guilherme Dantas <guidanoli@proton.me>
2025-01-21 01:01:58 -0300

    Group services by paid, unpaid and inactive

--- app/index.py
+++ app/index.py
@@ -1,28 +1,81 @@
-from datetime import datetime
+from datetime import date
+import math
 
 from flask import Blueprint, render_template, request, redirect, session, url_for
 
-from app.db import get_services
+from app.db import get_services, get_last_payments_for
+from app.time import estimate_next_payment_date
 
 bp = Blueprint('index', __name__)
 
 
+def days_until_estimated_payment_date(service, today):
+    service_id = service['service_id']
+    last_payments = get_last_payments_for(service_id)
+    estimated_payment_date = estimate_next_payment_date(service, last_payments)
+    if estimated_payment_date:
+        delta = estimated_payment_date - today
+        return delta.days
+
+
+def unpaid_service_key(unpaid_service):
+    if unpaid_service['days'] is not None:
+        return (unpaid_service['days'], unpaid_service['service']['name'])
+    else:
+        return (-math.inf, unpaid_service['service']['name'])
+
+
+def format_unpaid_service_message(unpaid_service):
+    days = unpaid_service['days']
+    if days is not None:
+        if days > 0:
+            return f'in {days} days'
+        elif days < 0:
+            return f'{-days} days ago'
+        else:
+            return 'today'
+
+
+def is_message_urgent(unpaid_service):
+    days = unpaid_service['days']
+    if days is not None:
+        return days <= 7
+    else:
+        return False
+
+
+def format_unpaid_service(unpaid_service):
+    return {
+        'service': unpaid_service['service'],
+        'message': format_unpaid_service_message(unpaid_service),
+        'is_message_urgent': is_message_urgent(unpaid_service),
+    }
+
+
 @bp.route('/')
 def index():
-    services = get_services()
+    unpaid_services = []
+    paid_services = []
+    inactive_services = []
+    today = date.today()
+
+    for service in get_services():
+        if not service['active']:
+            inactive_services.append(service)
+        elif not service['paid']:
+            days = days_until_estimated_payment_date(service, today)
+            unpaid_services.append({
+                'service': service,
+                'days': days,
+            })
+        else:
+            paid_services.append(service)
+
+    unpaid_services = sorted(unpaid_services, key=unpaid_service_key)
+    unpaid_services = list(map(format_unpaid_service, unpaid_services))
+
     kwargs = {}
-    kwargs['services'] = services
-    kwargs['show_inactive'] = session.get('show_inactive')
+    kwargs['unpaid_services'] = unpaid_services
+    kwargs['paid_services'] = paid_services
+    kwargs['inactive_services'] = inactive_services
     return render_template('index.html', **kwargs)
-
-
-@bp.route('/inactive/show')
-def show_inactive():
-    session['show_inactive'] = True
-    return redirect(url_for('index'))
-
-
-@bp.route('/inactive/hide')
-def hide_inactive():
-    session.pop('show_inactive', None)
-    return redirect(url_for('index'))
